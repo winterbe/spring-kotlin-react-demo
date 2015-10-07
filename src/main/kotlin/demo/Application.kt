e92c2f0259714763e11447e6c9ae592b6270f8a8
Benjamin Winterberg <winterbe@gmail.com>
2015-10-07 14:14:22 +0200

    Migrate to Kotlin M14

--- src/main/kotlin/demo/Application.kt
+++ src/main/kotlin/demo/Application.kt
@@ -2,17 +2,17 @@ package demo
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import kotlin.platform.platformStatic
 
 /**
  *
  * @author Benjamin Winterberg
  */
-SpringBootApplication
+@SpringBootApplication
 open class Application {
     companion object {
-        platformStatic fun main(args: Array<String>) {
-            SpringApplication.run(javaClass<Application>(), *args)
+        @JvmStatic
+        fun main(args: Array<String>) {
+            SpringApplication.run(Application::class.java, *args)
         }
     }
 }
--- src/main/kotlin/demo/GreetingController.kt
+++ src/main/kotlin/demo/GreetingController.kt
@@ -1,16 +1,15 @@
 package demo
 
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
-RestController
+@RestController
 class GreetingController {
 
     val idGenerator = AtomicLong()
 
-    RequestMapping("/hello")
+    @RequestMapping("/hello")
     fun hello(name: String): Greeting {
         val id = idGenerator.incrementAndGet()
         return Greeting(id, "$name, from Kotlin!")
