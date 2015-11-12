197730e7e3e31151e0244a5b0bd1553ff78cb8ee
Benjamin Winterberg <winterbe@gmail.com>
2015-11-12 12:18:23 +0100

    Simplify code

--- src/main/kotlin/demo/Application.kt
+++ src/main/kotlin/demo/Application.kt
@@ -3,16 +3,9 @@ package demo
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
-/**
- *
- * @author Benjamin Winterberg
- */
 @SpringBootApplication
-open class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(Application::class.java, *args)
-        }
-    }
+open class Application
+
+fun main(args: Array<String>) {
+    SpringApplication.run(Application::class.java, *args)
 }
